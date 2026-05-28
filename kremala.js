e0f1e640a14d795ecf31fbc409877f66417af131
kremala <64539989+kremala@users.noreply.github.com>
2026-05-28 03:51:20 +0300

    Update kremala.js

--- kremala.js
+++ kremala.js
@@ -1023,4 +1023,19 @@ var tainies = [
         } 
     }
 
-/**/
+/**/const scriptURL = 'https://script.google.com/macros/s/AKfycbycWBiwxd_TZMZAk-uHeVnZCubmMr0McvCXxiHiA957Tne0T5M/exec';
+
+function statistics(){
+    var form = new FormData();
+    form.append('ip(kremala)',ip);
+    form.append('innerWidth(kremala)',innerWidth);
+    form.append('innerHeight(kremala)',innerHeight);
+    form.append('screenWidth(kremala)',screen.width);
+    form.append('screenHeight(kremala)',screen.height);
+    fetch(scriptURL, { method: 'POST', body: form });
+}
+
+var ip;
+function getIP(json) {
+    ip = json.ip;
+  }
